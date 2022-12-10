f9bf2c78fd21a29c16d6d188a9077787b31a9619
neasher <neasherahmed75@gmail.com>
2022-12-10 22:56:32 +0600

    footer

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className="max-w-[992px] mx-auto my-12">
+        <div className="max-w-[992px] mx-auto mt-12">
             <div className='mx-6'>
-                <h2>Footer</h2>
+                <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+                    <div>
+                        <p>Copyright © 2022 - All right reserved by Neasher</p>
+                    </div>
+                </footer>
             </div>
         </div>
     );
